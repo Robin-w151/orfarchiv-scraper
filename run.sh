0c0ad82cc914d4b8e67aba72d07ca77ade283dd2
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2025-05-06 15:55:25 +0000

    refactor(run.sh): remove conditional database URL file export

--- run.sh
+++ run.sh
@@ -1,14 +1,7 @@
 #!/bin/bash -e
 
 scriptDir=$(dirname "$0")
-dbUrlFile="$1"
-
-if [ -n "$dbUrlFile" ]; then
-  ORFARCHIV_DB_URL_FILE="$dbUrlFile"
-  export ORFARCHIV_DB_URL_FILE
-fi
-
 export NODE_NO_WARNINGS=1
 
 cd "$scriptDir"
-timeout 60s npm start -- "$@"
+npm start -- "$@"
